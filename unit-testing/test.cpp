70ba97581de8aa1ac20901e747174baaffd0073c
KevinWDaley <kevin.daley1337@gmail.com>
2021-10-20 02:59:00 -0700

    ;)

--- unit-testing/test.cpp
+++ unit-testing/test.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char const *argv[]) {
   /* code */
-  ls
+  
   std::cout << "UNIT TESTING" << '\n';
   std::cout << "Function nginxPID:" << "\"" << nginxPID() << "\"" << '\n';
   std::cout << "Function getIpAddress:" << "\"" << getIpAddress() << "\"" << '\n';
